830363e8215857044a20a9b8c587f7de8166df75
kapilan1 <kapilan@codelantic.com>
2024-01-12 18:07:32 +0530

    jQuery effects added seperately

---
+++ jQueryTestIndex2.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    // $('#btn1').click(function () {       
+    //     $('#p1').hide(3000);                 // hide that element
+    // });
+
+    // $('#btn2').click(function () {
+    //     $('#p1').show(2000);                    // show that hided element
+    // });
+
+    // $('#btn3').click(function () {
+    //     $('#p1').toggle();                      // hide that elemnt and then show elemnt
+    // });
+
+    // $('#btn4').click(function () {
+    //     $('.div1').fadeOut();                   // hide elemet 
+    //     $('.div2').fadeOut("slow");
+    //     $('.div3').fadeOut(3000);
+    // });
+
+    // $('#btn5').click(function () {
+    //     $('.div4').fadeIn();                   // show hided elemet 
+    //     $('.div5').fadeIn("slow");
+    //     $('.div6').fadeIn(3000);
+    // });
+
+    // $('#btn6').click(function () {
+    //     $('.div7').fadeToggle();                   // show hided elemet and hide that again 
+    //     $('.div8').fadeToggle("slow");
+    //     $('.div9').fadeToggle(5000);
+    // });
+
+    // $('#btn7').click(function () {
+    //     $('.div10').fadeTo("fast",0.15);                   // decreases the opacity
+    //     $('.div11').fadeTo("slow",0.7);
+    //     $('.div12').fadeTo(5000,0.2);
+    // });
+});
