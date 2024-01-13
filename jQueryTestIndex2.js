54105d70e1dd093a00f8cf598ea8382620169eb5
kapilan1 <kapilan@codelantic.com>
2024-01-13 22:04:25 +0530

    annimations few completed

--- jQueryTestIndex2.js
+++ jQueryTestIndex2.js
@@ -1,37 +1,51 @@
 $(document).ready(function () {
-    // $('#btn1').click(function () {       
-    //     $('#p1').hide(3000);                 // hide that element
-    // });
+    $('#btn1').click(function () {       
+        $('#p1').hide(3000);                 // hide that element
+    });
 
-    // $('#btn2').click(function () {
-    //     $('#p1').show(2000);                    // show that hided element
-    // });
+    $('#btn2').click(function () {
+        $('#p1').show(2000);                    // show that hided element
+    });
 
-    // $('#btn3').click(function () {
-    //     $('#p1').toggle();                      // hide that elemnt and then show elemnt
-    // });
+    $('#btn3').click(function () {
+        $('#p1').toggle();                      // hide that elemnt and then show elemnt
+    });
 
-    // $('#btn4').click(function () {
-    //     $('.div1').fadeOut();                   // hide elemet 
-    //     $('.div2').fadeOut("slow");
-    //     $('.div3').fadeOut(3000);
-    // });
+    $('#btn4').click(function () {
+        $('.div1').fadeOut();                   // hide elemet 
+        $('.div2').fadeOut("slow");
+        $('.div3').fadeOut(3000);
+    });
 
-    // $('#btn5').click(function () {
-    //     $('.div4').fadeIn();                   // show hided elemet 
-    //     $('.div5').fadeIn("slow");
-    //     $('.div6').fadeIn(3000);
-    // });
+    $('#btn5').click(function () {
+        $('.div4').fadeIn();                   // show hided elemet 
+        $('.div5').fadeIn("slow");
+        $('.div6').fadeIn(3000);
+    });
 
-    // $('#btn6').click(function () {
-    //     $('.div7').fadeToggle();                   // show hided elemet and hide that again 
-    //     $('.div8').fadeToggle("slow");
-    //     $('.div9').fadeToggle(5000);
-    // });
+    $('#btn6').click(function () {
+        $('.div7').fadeToggle();                   // show hided elemet and hide that again 
+        $('.div8').fadeToggle("slow");
+        $('.div9').fadeToggle(5000);
+    });
 
-    // $('#btn7').click(function () {
-    //     $('.div10').fadeTo("fast",0.15);                   // decreases the opacity
-    //     $('.div11').fadeTo("slow",0.7);
-    //     $('.div12').fadeTo(5000,0.2);
-    // });
+    $('#btn7').click(function () {
+        $('.div10').fadeTo("fast",0.15);                   // decreases the opacity
+        $('.div11').fadeTo("slow",0.7);
+        $('.div12').fadeTo(5000,0.2);
+    });
+
+    $('#flip').click(function () { 
+        $("#panel").slideDown();
+        // $("#panel").slideDown(3000);        // slide down after 3 seconds (appear after 3 seconds by coming downwards)
+    });
+
+    $('#flip').click(function () { 
+        // $("#up").slideUp(3000);             // slide up after 3 seconds (hide after 3 seconds by going upwards)
+        $("#up").slideUp();            
+    });
+     
+    $('#flip').click(function () { 
+            $("#toggle").slideToggle("slow");             // hide and visible      
+        });
 });
